a062a4a0922f6f1efe88b04f1720c5eecc223485
kaseopea <kaseopea@gmail.com>
2018-08-01 06:19:49 +0300

    Start point

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const OPTIONS = {
+    distPath: path.resolve(__dirname, 'dist')
+};
+
+/* ---------------------------------- INDEX PAGE ---------------------------------- */
+const IndexPagePlugin = new HtmlWebpackPlugin({
+    template: './src/index.html'
+});
+
+/* ---------------------------------- MAIN CONFIG ---------------------------------- */
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: './src/js/index.js'
+    },
+    output: {
+        path: OPTIONS.distPath,
+        filename: '[name]-[hash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: 'html-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components|dist)/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [IndexPagePlugin]
+};
